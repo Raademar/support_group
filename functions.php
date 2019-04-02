2f70b88a083284802867ecae78af4d0bde87e382
Mattias Rådemar <mattiasrademar@gmail.com>
2019-04-02 09:11:31 +0200

    custom fields update.

--- functions.php
+++ functions.php
@@ -13,7 +13,7 @@ add_action('after_setup_theme', function () {
 require get_template_directory().'/post-types/branches.php';
 require get_template_directory().'/post-types/activities.php';
 
-// register custom fields for Student post type
+// // register custom fields for Student post type
 if (function_exists('acf_add_local_field_group')) {
   acf_add_local_field_group([
   	'key' => 'branch_info',
@@ -27,14 +27,14 @@ if (function_exists('acf_add_local_field_group')) {
   			'instructions' => 'Fill out the branch contact email',
   			'required' => 1,
   		],
-  		[
-  			'key' => 'branch_facebook_account',
-  			'label' => 'Facebook account for the branch',
-  			'name' => 'facebook_account_for_the_branch',
-  			'type' => 'url',
-  			'instructions' => 'Add the url to the branch Facebook account',
-  			'required' => 0,
-  		],
+//   		[
+//   			'key' => 'branch_facebook_account',
+//   			'label' => 'Facebook account for the branch',
+//   			'name' => 'facebook_account_for_the_branch',
+//   			'type' => 'url',
+//   			'instructions' => 'Add the url to the branch Facebook account',
+//   			'required' => 0,
+// 			],
   	],
   	'location' => [
   		[
@@ -47,4 +47,28 @@ if (function_exists('acf_add_local_field_group')) {
   	],
   ]);
 }
+
+add_action( 'init', 'my_branch_cpt' );
+function my_branch_cpt() {
+    $args = array(
+      'public'       => true,
+      'show_in_rest' => true,
+      'label'        => 'Branches'
+    );
+    register_post_type( 'branches', $args );
+}
+
+function my_rest_prepare_branches($data, $post, $request) {
+  $_data = $data->data;
+
+	$fields = get_fields($post->ID);
+	// die(var_dump($fields));
+  foreach ($fields as $key => $value){
+    $_data[$key] = get_field($key, $post->ID);
+  }
+	$data->data = $_data;
+	
+  return $data;
+}
+add_filter("rest_prepare_branches", 'my_rest_prepare_branches', 10, 3);
 
--- nextjs/pages/index.js
+++ nextjs/pages/index.js
@@ -8,10 +8,13 @@ export default () => (
 		<Navigation />
 		<Head>
 			<title>This is our page title!</title>
-			<meta name="description" content="This is an example of a meta description. This will show up in search results." />
+			<meta
+				name="description"
+				content="This is an example of a meta description. This will show up in search results."
+			/>
 			<meta charSet="utf-8" />
 			<meta name="viewport" content="initial-scale=1.0, width=device-width" />
 		</Head>
-			<h1>Our home page!</h1>
+		<h1>Our home page!</h1>
 	</Fragment>
 )
