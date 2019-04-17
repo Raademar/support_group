895ebc7c022c593a860f77d5e54a326576ac3f1f
Mattias Rådemar <mattiasrademar@gmail.com>
2019-04-17 15:25:09 +0200

    started working on posts page.

--- nextjs/components/Header/index.js
+++ nextjs/components/Header/index.js
@@ -1,76 +1,82 @@
+import React, { Component } from 'react'
 import Navigation from '../Navigation'
 import HamburgerBar from '../HamburgerBar'
 import Logo from '../Logo'
 import styled from 'styled-components'
-import React from 'react'
 import Head from 'next/head'
 
 const StyledHeader = styled.div`
 	background-color: #3d73cf;
-	height: 76px;
 	display: grid;
 	grid-template-columns: repeat(5, 20%);
+	/* position: absolute;
+	top: 0; */
+	transition: top 0.2s ease-in-out;
+	width: 100%;
 `
-
-const Header = props => {
-	{
+class Header extends Component {
+	constructor(props) {
+		super(props)
 	}
-	return (
-		<StyledHeader>
-			<Head>
-				<meta name="viewport" content="width=device-width, initial-scale=1" />
-				<meta charSet="utf-8" />
-				<link
-					href="https://fonts.googleapis.com/css?family=Lato:300,400,700"
-					rel="stylesheet"
-				/>
-				<link
-					href="https://fonts.googleapis.com/css?family=Paytone+One"
-					rel="stylesheet"
-				/>
-				<link
-					rel="stylesheet"
-					type="text/css"
-					charset="UTF-8"
-					href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css"
-				/>
-				<link
-					rel="stylesheet"
-					type="text/css"
-					href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css"
-				/>
-			</Head>
-			<style jsx global>{`
-				body {
-					padding: 0;
-					margin: 0;
-					font-family: 'Lato', sans-serif;
-					overflow-x: hidden;
-				}
-				.slide-container {
-					background: #fff;
-				}
-				.slick-slide h3,
-				.slick-slide p,
-				.slick-slide span {
-					padding: 0 24px;
-				}
-				.slick-slide img {
-					width: 100%;
-				}
-			`}</style>
-			<Logo images={props.images} />
-			{props.isDesktop ? (
-				<Navigation
-					navigationMenu={props.navigationMenu}
-					gridCol="2/6"
-					flexDir="row"
-				/>
-			) : (
-				<HamburgerBar onClick={props.toggleMenu} />
-			)}
-		</StyledHeader>
-	)
+
+	render() {
+		return (
+			<StyledHeader>
+				<Head>
+					<meta name="viewport" content="width=device-width, initial-scale=1" />
+					<meta charSet="utf-8" />
+					<link
+						href="https://fonts.googleapis.com/css?family=Lato:300,400,700"
+						rel="stylesheet"
+					/>
+					<link
+						href="https://fonts.googleapis.com/css?family=Paytone+One"
+						rel="stylesheet"
+					/>
+					<link
+						rel="stylesheet"
+						type="text/css"
+						charset="UTF-8"
+						href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick.min.css"
+					/>
+					<link
+						rel="stylesheet"
+						type="text/css"
+						href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.6.0/slick-theme.min.css"
+					/>
+				</Head>
+				<style jsx global>{`
+					body {
+						padding: 0;
+						margin: 0;
+						font-family: 'Lato', sans-serif;
+						overflow-x: hidden;
+					}
+					.slide-container {
+						background: #fff;
+					}
+					.slick-slide h3,
+					.slick-slide p,
+					.slick-slide span {
+						padding: 0 24px;
+					}
+					.slick-slide img {
+						width: 100%;
+					}
+				`}</style>
+				<Logo images={this.props.images} />
+				{this.props.isDesktop ? (
+					<Navigation
+						navigationMenu={this.props.navigationMenu}
+						gridCol="2/6"
+						flexDir="row"
+					/>
+				) : (
+					<HamburgerBar onClick={this.props.toggleMenu} />
+				)}
+			</StyledHeader>
+		)
+	}
 }
 
 export default Header
--- nextjs/pages/posts.js
+++ nextjs/pages/posts.js
@@ -1,37 +1,127 @@
-import Navigation from '../components/Navigation'
-import { Component, Fragment } from 'react'
-import styled from 'styled-components'
+import React, { Component } from 'react'
 import axios from 'axios'
-import Layout from '../compenents/Layout'
+import Layout from '../components/Layout'
+import Heading from '../components/Heading'
+import styled from 'styled-components'
+
+const StyledNewsPosts = styled.div`
+	background-color: #dfeafa;
+	display: flex;
+	flex-direction: column;
+	align-items: center;
+	justify-content: center;
+	padding: 24 0px;
+	.news-container {
+		background-color: #fff;
+		width: 100vw;
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		margin-bottom: 42px;
+		.news-image-container {
+			padding: 24px 0;
+			margin: 0 24px;
+			img {
+				width: 100%;
+				height: auto;
+			}
+		}
+		.news-content-container {
+			width: 100%;
+			font-size: 18px;
+			line-height: 30px;
+			letter-spacing: 0.04em;
+			p {
+				font-weight: 200;
+			}
+		}
+		.news-content-container > * {
+			margin: 8px 24px;
+		}
+		.news-date {
+			width: 100%;
+			color: #3d73cf;
+			margin: 24px 0px;
+			font-weight: 800;
+		}
+		.news-date > * {
+			margin: 8px 24px;
+		}
+	}
+`
 
 export default class extends Component {
 	// Resolve promise and get initial props
-	static async getInitialProps() {
+	static async getInitialProps({ req }) {
 		// Make request for props
+
+		const posts = await axios.get(
+			`http://${process.env.HOSTNAME}/wp-json/wp/v2/posts`
+		)
 		const pages = await axios.get(
 			`http://${process.env.HOSTNAME}/wp-json/menus/v2/header`
 		)
-		const branches = await axios.get(
-			`http://${process.env.HOSTNAME}/wp-json/wp/v2/branches`
-		)
-
 
 		// Return the data
 
 		return {
-			pages: pages.data,
-			branches: branches.data
+			posts: posts.data,
+			pages: pages.data
 		}
 	}
+
+	getExcerpted = (str, limit) => {
+		let fullText = str
+		let shortText = str
+		shortText = shortText.substr(0, shortText.lastIndexOf(' ', limit)) + '...'
+		let returnString = {
+			fullText: fullText,
+			shortText: shortText
+		}
+		return returnString
+	}
+
+	parseShitWPTimeToNormalTime = badTime => {
+		Date.parse(badTime)
+		const goodTime = new Date(badTime)
+		const date = goodTime.getDate()
+		const month = goodTime.getMonth()
+		const hours = goodTime.getHours()
+
+		// Will display time in 10: format
+		const formattedTime = `${date}/${month} ${hours}:00`
+		return formattedTime
+	}
+
 	render() {
+		const posts = this.props.posts
+		console.log(posts)
+
 		return (
-			<Layout>
-				<h1>Our Posts Page!</h1>
-				<ul>
-					{this.props.branches.map(item => (
-						<li key={item.id}>{item.title.rendered}</li>
+			<Layout navigationMenu={this.props.pages}>
+				<Heading heroText="NEWS" position="initial" />
+				<StyledNewsPosts>
+					{posts.map((item, index) => (
+						<div className="news-container">
+							<div className="news-image-container">
+								<img src={item.acf.image.sizes.large} alt="" />
+							</div>
+							<div className="news-content-container">
+								<h3 className="news-header">{item.title.rendered}</h3>
+								<p className="news-content">
+									{this.getExcerpted(item.acf.text, 40).shortText}
+								</p>
+							</div>
+							<div className="news-date">
+								<span>
+									{this.parseShitWPTimeToNormalTime(item.date)}{' '}
+									{item.acf.specific_branch}
+								</span>
+							</div>
+						</div>
 					))}
-				</ul>
+				</StyledNewsPosts>
 			</Layout>
 		)
 	}
